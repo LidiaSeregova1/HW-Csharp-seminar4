0d0f779de32c2d75ba972b47b6253b36f1760f41
LidiaSeregova <Lidia.199855@gmail.com>
2022-12-12 12:05:34 +0300

    first commit

---
+++ task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную 
+// степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите еще одно число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int res = number1;
+
+for (int i = 2; i <= number2; i++)
+{
+    res *= number1;
+}
+
+Console.WriteLine($"{number1}^{number2} = {res}");
---
+++ task29/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+// int[] array = new int [8];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = int.Parse(Console.ReadLine());
+    
+// }
+// Console.WriteLine(string.Join(", ", array));
+
+
+            Console.WriteLine("Введите восьмизначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                array[i] = new Random().Next(number+1);
+                Console.Write(array[i] + "; ");
+            }
+            Console.WriteLine();
